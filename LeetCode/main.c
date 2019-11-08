863705c911744b16830a4b4d6e423a7075b79764
qinzhiwei <qinzhiwei@58.com>
2019-11-08 12:46:35 +0800

    init

---
+++ LeetCode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LeetCode
+//
+//  Created by 秦志伟 on 2019/11/8.
+//  Copyright © 2019 lobster. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
