d5a63f873b5c8433d2d3a4dacc68734ff853e9fd
Lobster-King <zhiwei.geek@gmail.com>
2019-11-18 16:02:37 +0800

    java project

---
+++ src/com/lobster/_19_删除链表的倒数第N个节点.java
@@ -0,0 +1,11 @@
+package com.lobster;
+
+public class _19_删除链表的倒数第N个节点 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        return null;
+    }
+    public static void main(String[] args) {
+
+    }
+}
